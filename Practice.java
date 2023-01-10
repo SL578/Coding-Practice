88409d8cfcf433939228ca066f4ab2f50dc99cba
SL578 <119895808+SL578@users.noreply.github.com>
2023-01-09 18:17:36 -0800

    01092023

--- Practice.java
+++ Practice.java
@@ -654,3 +654,14 @@ public int[] makePi() {
   makePi[2] = 4;
   return makePi;
 }
+
+// 01092023
+
+public boolean commonEnd(int[] a, int[] b) {
+   if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]){
+     return true;
+   }
+   else{
+     return false;
+   }
+}
