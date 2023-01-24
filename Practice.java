aee598f7efa96067de5544b64a07930980aca610
SL578 <119895808+SL578@users.noreply.github.com>
2023-01-23 16:47:36 -0800

    01232023

--- Practice.java
+++ Practice.java
@@ -700,3 +700,17 @@ public int[] maxEnd3(int[] nums) {
   }
   return rtrnums;
 }
+
+// 01232023
+
+public int sum2(int[] nums) {
+  if (nums.length > 1){
+    return nums[0] + nums[1];
+  }
+  else if (nums.length > 0){
+    return nums[0];
+  }
+  else{
+    return 0;
+  }
+}
