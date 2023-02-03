79c6b032d4451da369b5920e4ff10b0d27e35a14
SL578 <119895808+SL578@users.noreply.github.com>
2023-02-03 15:32:51 -0800

    02032023

--- Practice.java
+++ Practice.java
@@ -751,3 +751,21 @@ public boolean no23(int[] nums) {
     return true;
   }
 }
+
+public int[] makeLast(int[] nums) {
+  int[] rtrInt = new int[nums.length * 2];
+  rtrInt[rtrInt.length - 1] = nums[nums.length - 1];
+  return rtrInt;
+}
+
+public boolean double23(int[] nums) {
+  if (nums.length == 0 || nums.length == 1){
+    return false;
+  }
+  else if (nums.length == 2 && (nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3)){
+    return true;    
+  }
+  else{
+    return false;
+  }
+}
