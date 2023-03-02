c8e72c853c5f11178535911b1e48340730de5471
SL578 <119895808+SL578@users.noreply.github.com>
2023-03-01 18:19:37 -0800

    03012023

--- Practice.java
+++ Practice.java
@@ -868,3 +868,19 @@ public int[] frontPiece(int[] nums) {
   }
   return rtrInt;
 }
+
+// 03012023
+public boolean unlucky1(int[] nums) {
+  if (nums.length > 1 && nums[0] == 1 && nums[1] == 3){
+    return true;
+  }
+  if (nums.length > 1 && nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3){
+    return true;
+  }
+  if (nums.length > 2 && nums[1] == 1 && nums[2] == 3){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
