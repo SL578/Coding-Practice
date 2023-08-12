1621228b0afe66e6cd89678bb337722d5a9b4d78
SL578 <119895808+SL578@users.noreply.github.com>
2023-08-12 10:25:00 -0700

    Update and rename CodewarsC#Practice to CodewarsC#Practice.cs

---
+++ CodewarsC#Practice.cs
@@ -0,0 +1,9 @@
+// 08122023 https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/csharp
+public class Kata
+{
+  public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+  {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+}
+
