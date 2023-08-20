8c6f4617a5b9c49a2629dfbfdff6e875087271da
SL578 <119895808+SL578@users.noreply.github.com>
2023-08-19 21:55:44 -0700

    Update CodewarsC#Practice.cs

--- CodewarsC#Practice.cs
+++ CodewarsC#Practice.cs
@@ -105,3 +105,14 @@ public class Kata
     return num.ToString();
   }
 }
+
+// 08192923 https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/csharp
+using System;
+
+public static class Kata 
+{
+    public static int la_liga_goals = 43;
+    public static int champions_league_goals = 10;
+    public static int copa_del_rey_goals = 5;
+    public static int total_goals = 58;
+}
